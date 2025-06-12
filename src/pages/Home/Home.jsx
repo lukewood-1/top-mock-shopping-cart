6ea2fabdb9f7859d8c7ef09de02784fd90bc621d
Lucas Lopes <173523829+lukewood-1@users.noreply.github.com>
2025-06-12 04:44:59 -0300

    feat: Design Home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,15 +3,15 @@ import styles from './Home.module.css';
 
 function Home(){
   return (<>
-    <div className={styles.heroSection}>
-      <div className={styles.left}>
-        <h1 className={styles.headline}>Test Shop</h1>
-        <div className={styles.btnDiv}>
+    <div className={styles.wrapper}>
+      <div className={styles.heroSection}>
+        <div className={styles.left}>
+          <h1 className={styles.headline}>Test Shop</h1>
           <button className={styles.link}><Link to='shop'>Shop now</Link></button>
         </div>
-      </div>
-      <div className={styles.right}>
-        <img src="/hero-image.png" alt="image" width='150' height='150' />
+        <div className={styles.right}>
+          <img className={styles.heroImage} src="/hero-image.png" alt="image" width='150' height='150' />
+        </div>
       </div>
     </div>
   </>)
